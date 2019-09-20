36f7023c4a01102dc1db8d79525932a379532034
Kristen <hackettka@gmail.com>
2019-09-20 12:20:25 -0400

    map view set to queens

--- leaflet.js
+++ leaflet.js
@@ -1,10 +1,10 @@
-let map, tileLayer;
-map = L.map("map"), {};
+let qnsmap, tileLayer;
+qnsmap = L.map("qns-map");
 tileLayer =
 L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
       attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy; <a href='http://carto.com/attribution'>CARTO</a>",
       subdomains: "abcd",
       maxZoom: 18
     });
-    tileLayer.addTo(firstMap);
-    map.setView([40.761884, -73.911642], 13);
+    tileLayer.addTo(qnsmap);
+    qnsmap.setView([40.761884, -73.911642], 13);
