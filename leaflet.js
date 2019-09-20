16a8610c1ca01c7149455ec7348d1cd77906a3be
Kristen <hackettka@gmail.com>
2019-09-20 11:52:04 -0400

    migrating exp map content

---
+++ leaflet.js
@@ -0,0 +1,10 @@
+let map, tileLayer;
+map = L.map("map"), {};
+tileLayer =
+L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
+      attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy; <a href='http://carto.com/attribution'>CARTO</a>",
+      subdomains: "abcd",
+      maxZoom: 18
+    });
+    tileLayer.addTo(firstMap);
+    map.setView([40.761884, -73.911642], 13);
